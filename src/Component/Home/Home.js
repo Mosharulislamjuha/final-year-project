0647ba1333de737e37ca9fbfb35b22c33d313f61
Mosharulislamjuha <juhacse1@gmail.com>
2022-10-30 00:48:31 +0600

    first commit

---
+++ src/Component/Home/Home.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import Front from '../Front/Front';
+
+const Home = () => {
+    return(
+        <div>
+            <Front/>
+        </div>
+    )
+}
+export default Home;
