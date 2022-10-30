4eeaa45fcc09bc8d867f8b854b9aa493d3986298
Mosharulislamjuha <juhacse1@gmail.com>
2022-10-31 00:09:06 +0600

    3rd

--- src/Component/Home/Home.js
+++ src/Component/Home/Home.js
@@ -1,10 +1,12 @@
 import React from 'react'
 import Front from '../Front/Front';
+import Midle from '../Midle/Midle';
 
 const Home = () => {
     return(
         <div>
             <Front/>
+            <Midle/>
         </div>
     )
 }
